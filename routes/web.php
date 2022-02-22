c921cb8812192c5b555f3459e1fdc51031c0c80f
NyiZinThant <codebynyi@gmail.com>
2022-02-22 15:33:03 +0630

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Product\ProductController;
+
+Route::get('/', [ArticleController::class, 'index']);
+
+Route::get('/articles', [ArticleController::class, 'index']);
+
+Route::get('/articles/detail', function () {
+    return "Article Detail";
+})->name("article.detail");
+
+Route::get('/articles/detail/{id}', [
+    ArticleController::class,
+    'detail'
+])->name("articles.detail");
+
+Route::get('/articles/more', function () {
+    return redirect()->route('article.detail');
+});
+
+Route::get('/products', [ProductController::class, "index"]);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/articles/add', [ArticleController::class, 'add']);
+
+Route::post('/articles/add', [ArticleController::class, 'create']);
+
+Route::get('/articles/delete/{id}', [
+    ArticleController::class,
+    'delete'
+]);
